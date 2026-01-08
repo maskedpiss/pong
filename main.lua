0fd77aba829c072502fee3cf00bb3200c21af874
maskedpiss <joshkeckedits@proton.me>
2026-01-08 12:09:44 -0600

    drew paddle

--- main.lua
+++ main.lua
@@ -1,17 +1,22 @@
 Globals = {}
 Globals.Arena = nil
+Globals.Paddle = nil
 
 function love.load()
   Globals.Arena = require("src/objs/arena")
   Globals.Arena:load()
+  
+  Globals.Paddle = require("src/objs/paddle")
+  Globals.Paddle:load()
 end
 
 
 function love.update(dt)
-  
+  Globals.Paddle:update(dt)
 end
 
 
 function love.draw()
   Globals.Arena:draw()
+  Globals.Paddle:draw()
 end
--- src/objs/paddle.lua
+++ src/objs/paddle.lua
@@ -1,7 +1,10 @@
 local Paddle = {}
 
 function Paddle:load()
-  
+  self.x = 50
+  self.y = love.graphics.getHeight() / 2
+  self.width = 20
+  self.height = 100
 end
 
 
@@ -11,7 +14,8 @@ end
 
 
 function Paddle:draw()
-  
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
 end
 
 return Paddle
