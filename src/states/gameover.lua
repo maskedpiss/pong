bc1a28be581489e4ed7a97ffbb3b122990c91768
maskedpiss <joshkeckedits@proton.me>
2026-01-09 18:19:16 -0600

    drew message for game over screen

--- src/states/gameover.lua
+++ src/states/gameover.lua
@@ -1,7 +1,12 @@
 local GameOver = {}
 
 function GameOver.onEnter()
-  
+  GameOver.Message = {
+      font = love.graphics.newFont(128),
+      x = 0,
+      y = 100,
+      text = "Game Over!"
+  }
 end
 
 
@@ -11,7 +16,9 @@ end
 
 
 function GameOver.draw()
-  
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.setFont(GameOver.Message.font)
+  love.graphics.printf(GameOver.Message.text, GameOver.Message.x, GameOver.Message.y, love.graphics.getWidth(), "center")
 end
 
 
