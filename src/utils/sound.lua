367c59741c159261559ef2c75c497c3db1c0d089
maskedpiss <joshkeckedits@proton.me>
2026-01-13 12:56:09 -0600

    loaded sfx files in Sound:loadSound

--- src/utils/sound.lua
+++ src/utils/sound.lua
@@ -1,7 +1,13 @@
 local Sound = {}
 
 function Sound:loadSound()
-  
+  self.ButtonHover = love.audio.newSource("res/sfx/ButtonHover.wav", "static")
+  self.ButtonClick = love.audio.newSource("res/sfx/ButtonClick.wav", "static")
+  self.HitPaddle1 = love.audio.newSource("res/sfx/HitPaddle1.wav", "static")
+  self.HitPaddle2 = love.audio.newSource("res/sfx/HitPaddle2.wav", "static")
+  self.HitCeiling = love.audio.newSource("res/sfx/HitCeiling.wav", "static")
+  self.HitFloor = love.audio.newSource("res/sfx/HitFloor.wav", "static")
+  self.Goal = love.audio.newSource("res/sfx/Goal.wav", "static")
 end
 
 return Sound
